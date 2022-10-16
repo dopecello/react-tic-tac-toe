f01a0166412f59f56d61f7e0be1e89f07ad1c91c
dopecello <williamlocke.cello@gmail.com>
2022-10-16 10:10:35 -0400

    added historical move feature to the game

--- src/index.js
+++ src/index.js
@@ -54,12 +54,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -68,14 +69,33 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "❌" : "⭕";
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li> // it is strongly recommended that you assign proper keys whenever you build dynamic lists.
+        // additionally, keys do not need to be globally unique; they only need to be unique between components and their siblings.
+      );
+    });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -86,13 +106,14 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
-          squares={current.squares}
-          onClick={(i) => this.handleClick(i)}/>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
